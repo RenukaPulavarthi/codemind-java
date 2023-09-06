b4514c742fc1cf998e5ed63f9f80ba8474b8d49f
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2023-09-06 11:24:37 +0530

    import java.util.*; public class Arr{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();             if(arr[i]%2==0)             {                 System.out.print(arr[i]+" ");             }         }         for(int i=0;i<n;i++)         {             if(arr[i]%2!=0)             {                System.out.print(arr[i]+" ");             }         }
        }
    }

---
+++ Even_elements_followed_by_Odd_elements.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Arr{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+            if(arr[i]%2==0)
+            {
+                System.out.print(arr[i]+" ");
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]%2!=0)
+            {
+               System.out.print(arr[i]+" ");
+            }
+        }
+        
+    }
+}
