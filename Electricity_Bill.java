6df540e8e3f1a4ad666e3c0fc06e5ea4fd47276d
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2023-10-22 15:22:43 +0530

    import java.util.*; public class ElectricityBill{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         System.out.println("Units Consumed: "+a);         double c;         if(a<=199)  c=1.2;         else if(a<400)  c=1.4;         else if(a<600)  c=1.6;         else if(a<800)  c=1.8;         else    c=2;         System.out.printf("Cost per Unit: %.2f ",c);         double b=c*a;         System.out.printf("Bill: %.2f ",b);         double s=0;         if(b>400)             s=b*0.15;         System.out.printf("Surcharge: %.2f ",s);         System.out.printf("Total Amount: %.2f ",s+b);     } }

--- Electricity_Bill.java
+++ Electricity_Bill.java
@@ -1,34 +1,27 @@
-import java.util.Scanner;
-public class Electricity{
+import java.util.*;
+public class ElectricityBill{
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        String s=sc.nextLine();
-        String ch=sc.nextLine();
-        int n=sc.nextInt();
+        int a=sc.nextInt();
+        System.out.println("Units Consumed: "+a);
         double c;
-        if(n<=199)
-        {
-            c=n*1.2;
-        }
-        else if(n<400)
-        {
-            c=n*1.5;
-        }
-        else if(n<600)
-        {
-            c=n*1.8;
-        }
-        else{
-            c=n*2.0;
-        }
-        if(c<400)
-        {
-            c+=100;
-        }
-        else{
-            c+=(c*0.15);
-        }
-        System.out.printf("%.2f",c);
+        if(a<=199)  c=1.2;
+        else if(a<400)  c=1.4;
+        else if(a<600)  c=1.6;
+        else if(a<800)  c=1.8;
+        else    c=2;
+        System.out.printf("Cost per Unit: %.2f
+",c);
+        double b=c*a;
+        System.out.printf("Bill: %.2f
+",b);
+        double s=0;
+        if(b>400)
+            s=b*0.15;
+        System.out.printf("Surcharge: %.2f
+",s);
+        System.out.printf("Total Amount: %.2f
+",s+b);
     }
 }
